95f650a93fc05ee7ecbda2759894819dd48a5b73
MarioCakeDev <davidleinweber@web.de>
2021-03-13 17:43:10 +0100

    yooo

--- Enemy.gd
+++ Enemy.gd
@@ -1,4 +1,16 @@
 extends KinematicBody2D
+class_name Enemy
+
+var rotationSpeed = 2
+var rotationSpeed = 2
+var rotationSpeed = 2
+
+func _ready():
+	rotationSpeed = rand_range(1, 4)
+	+ = rand_range()
 
 func onShot():
 	queue_free()
+	
+func _physics_process(delta):
+	rotate(rotationSpeed * delta)
