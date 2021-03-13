c6f1a0d19a17c8a12b19c7c950c4c7530905370d
MarioCakeDev <davidleinweber@web.de>
2021-03-13 20:01:53 +0100

    More frog deaths

--- Enemy.gd
+++ Enemy.gd
@@ -5,6 +5,9 @@ var rotationSpeed = 2
 var velocity: Vector2 = Vector2.DOWN * 100
 
 func onShot():
+	$Death.play()
+	position.x = -10000
+	yield(get_tree().create_timer($Death.stream.get_length()), "timeout")
 	queue_free()
 	
 func _physics_process(delta):
